a756643cd5f8b0bacba4f94f5da6bd7dfa3e6213
zlq4863947 <zlq4863947@gmail.com>
2019-11-10 22:29:26 +0900

    分离演示网页

--- assets/Script/utils/set-mouse-cursor.ts
+++ assets/Script/utils/set-mouse-cursor.ts
@@ -1,3 +1,4 @@
 export function setMouseCursor(canvas: HTMLCanvasElement): void {
-  canvas.style.cursor = "url('/res/import/ce/ce41cd0d-d754-49e4-afc0-e3dc17d1514b.png'),auto";
+  const path = cc.url.raw('resources/cursor.png');
+  canvas.style.cursor = `url('${path}'),auto`;
 }
