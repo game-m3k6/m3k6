cffff58b56d3e089ede8f8f351bba681f6b62929
zlq4863947 <zlq4863947@gmail.com>
2019-11-16 22:44:44 +0900

    地图更新

--- assets/texture/map/city.tsx
+++ assets/texture/map/city.tsx
@@ -1,7 +1,118 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="city" tilewidth="86" tileheight="54" tilecount="1" columns="0">
+<tileset version="1.2" tiledversion="1.2.5" name="city" tilewidth="380" tileheight="234" tilecount="38" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
+  <image width="86" height="63" source="city/0-1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="163" height="114" source="city/0-2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="216" height="138" source="city/0-3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="272" height="178" source="city/0-4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="316" height="198" source="city/0-5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="377" height="232" source="city/0-6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="86" height="63" source="city/1-1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="159" height="114" source="city/1-2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="198" height="138" source="city/1-3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="260" height="180" source="city/1-4.png"/>
+ </tile>
+ <tile id="10">
+  <image width="328" height="198" source="city/1-5.png"/>
+ </tile>
+ <tile id="11">
+  <image width="380" height="234" source="city/1-6.png"/>
+ </tile>
+ <tile id="12">
+  <image width="86" height="61" source="city/2-1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="147" height="85" source="city/2-2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="219" height="127" source="city/2-3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="272" height="149" source="city/2-4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="86" height="60" source="city/3-1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="150" height="83" source="city/3-2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="210" height="129" source="city/3-3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="264" height="151" source="city/3-4.png"/>
+ </tile>
+ <tile id="20">
+  <image width="86" height="72" source="city/4-1.png"/>
+ </tile>
+ <tile id="21">
+  <image width="163" height="116" source="city/4-2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="217" height="168" source="city/4-3.png"/>
+ </tile>
+ <tile id="23">
+  <image width="273" height="187" source="city/4-4.png"/>
+ </tile>
+ <tile id="24">
+  <image width="316" height="208" source="city/4-5.png"/>
+ </tile>
+ <tile id="25">
+  <image width="86" height="72" source="city/5-1.png"/>
+ </tile>
+ <tile id="26">
+  <image width="159" height="115" source="city/5-2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="198" height="170" source="city/5-3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="266" height="189" source="city/5-4.png"/>
+ </tile>
+ <tile id="29">
+  <image width="328" height="211" source="city/5-5.png"/>
+ </tile>
+ <tile id="30">
+  <image width="86" height="54" source="city/6-1.png"/>
+ </tile>
+ <tile id="31">
+  <image width="153" height="97" source="city/6-2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="221" height="136" source="city/6-3.png"/>
+ </tile>
+ <tile id="33">
+  <image width="278" height="162" source="city/6-4.png"/>
+ </tile>
+ <tile id="34">
   <image width="86" height="54" source="city/7-1.png"/>
+ </tile>
+ <tile id="35">
+  <image width="141" height="101" source="city/7-2.png"/>
+ </tile>
+ <tile id="36">
+  <image width="224" height="137" source="city/7-3.png"/>
+ </tile>
+ <tile id="37">
+  <image width="275" height="162" source="city/7-4.png"/>
  </tile>
 </tileset>
