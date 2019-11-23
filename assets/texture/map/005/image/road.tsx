87e413f912085ea40912e7a182ee6dc9f5a8199b
zlq4863947 <zlq4863947@gmail.com>
2019-11-23 11:12:30 +0900

    测试tiled

--- assets/texture/map/005/image/road.tsx
+++ assets/texture/map/005/image/road.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="road" tilewidth="70" tileheight="38" tilecount="4" columns="0">
+<tileset version="1.2" tiledversion="1.3.0" name="road" tilewidth="70" tileheight="38" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="32" source="road/base.png"/>
